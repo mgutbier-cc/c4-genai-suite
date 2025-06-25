d66266d147b18a97ecf2c76c0229ca2452ece801
Christopher Schädlich <christopher.schaedlich@codecentric.de>
2025-06-25 12:24:45 +0200

    chore: build library

---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+export * from 'src/domain/auth/interfaces';
+export * from 'src/domain/chat/interfaces';
+export * from 'src/domain/extensions/interfaces';
+export * from 'src/domain/files/interfaces';
+export * from 'src/domain/shared';
